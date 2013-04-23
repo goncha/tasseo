75f603390b2f73fba8095d139e461854a99424c0
Gang Chen <goncha@gmail.com>
2013-04-23 17:13:27 +0900

    Improve IE compatibility

--- public/j/tasseo.js
+++ public/j/tasseo.js
@@ -1,3 +1,12 @@
+// BEGIN: IE
+window.console = window.console || (function(){
+  var c = {}; c.log = c.warn = c.debug = c.info = c.error = c.time = c.dir = c.profile = c.clear = c.exception = c.trace = c.assert = function(){};
+  return c;
+})();
+
+jQuery.support.cors = true;
+// END: IE
+
 $(function () {
 
 var graphs;           // plot object
